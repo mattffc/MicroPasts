aa80d4d547eb6c66481b39069c27e307c352d3ff
Rich Wareham <rjw57@cam.ac.uk>
2015-11-02 13:22:29 +0000

    sobelise: add process_image function which performs pyramid decomposition

--- scripts/sobelise.py
+++ scripts/sobelise.py
@@ -1,6 +1,7 @@
 import numpy as np
 from skimage.filters import gaussian_filter, sobel_h, sobel_v
-from skimage.transform import rescale
+from skimage.transform import rescale, resize
+from PIL import Image
 
 def downsample(I):
     """Downsample I by 2 after blurring with Gaussian of sigma=2.
@@ -21,4 +22,15 @@ def sobel_rgb(I):
         [sobel_h(I[..., c]) for c in range(I.shape[-1])] +
         [sobel_v(I[..., c]) for c in range(I.shape[-1])]
     )
+
+def process_image(image_fn, levels=5):
+    im = Image.open(image_fn)
+    im = np.asarray(im.convert('RGB')) * (1.0/255.0)
+    orig_shape = im.shape[:2]
+    for level in range(levels):
+        s = resize(sobel_rgb(im), orig_shape)
+        s = (255 * (0.5 + s)).astype(np.uint8)
+        Image.fromarray(s[..., :3]).save('{}_h.png'.format(level))
+        Image.fromarray(s[..., 3:]).save('{}_v.png'.format(level))
+        im = downsample(im)
 
